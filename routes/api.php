48698cc05ecd48f11ef4a6a199697207dc76024b
josepmurciasampayo <josepmurciasampayo@gmail.com>
2022-05-13 15:33:21 -0400

    fixing controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/chat', 'ChatbotController@listenToReply');
+Route::post('/chat', [ChatbotController::class, 'listenToReply']);
