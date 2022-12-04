790f29c17edc6df58dbbf8c2f9c14b098b10ec67
josepmurciasampayo <josepmurciasampayo@gmail.com>
2022-12-04 15:48:24 +0000

    update to counselor home layout and notes design

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,26 +5,42 @@
     <div> 
     </div>
     <div>
-    <ul class="mb-16">
-          <div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-message"></i> <a href="{{ route('campaigns') }}">Chat language</a></button></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><li><li><i class="fas fa-book"></i> <a href="{{ route('comms-log') }}">WhatsApp log</a></button></li></div>
-            <br/>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-question"></i> <a href="{{ route('questions') }}">Questions</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-user"></i> <a href="{{ route('students') }}">Student data</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-handshake"></i> <a href="{{ route('matchData') }}">Match data</a></li></div>
-            <br/>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-building"></i> <a href="{{ route('universities') }}">Universities</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-school"></i> <a href="{{ route('highschools') }}">High Schools & Programs</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-chart-bar"></i> <a href="{{ route('reports') }}" target="_blank" >Reports</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-network-wired"></i> <a href="{{ route('workRequest') }}" target="_blank">Work Requests</a></li></div>
+        <div class="container-md border">
+            <div class="row">
+            <div class="card col-9">
+                <iframe class="card-img-top" width="600" height="450" src="https://datastudio.google.com/embed/reporting/1ac8ccdd-a501-495d-9947-e14b9c75e715/page/iqd8C" frameborder="0" style="border:0" allowfullscreen></iframe>            <div class="card-body">
+    
+            </div>
+                    
+</div>
+<div class="card col-3">
+    <div class="card-body">
+        <ul class="mb-16">
+              <div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-message"></i> <a href="{{ route('campaigns') }}">Chat language</a></button></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-book"></i> <a href="{{ route('comms-log') }}">WhatsApp log</a></button></li></div>
+                <br/>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-question"></i> <a href="{{ route('questions') }}">Questions</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-user"></i> <a href="{{ route('students') }}">Student data</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-handshake"></i> <a href="{{ route('matchData') }}">Match data</a></li></div>
+                <br/>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-building"></i> <a href="{{ route('universities') }}">Universities</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-school"></i> <a href="{{ route('highschools') }}">High Schools & Programs</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-chart-bar"></i> <a href="{{ route('reports') }}" target="_blank" >Reports</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-network-wired"></i> <a href="{{ route('workRequest') }}" target="_blank">Work Requests</a></li></div>
+    
+                <br/>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-database"></i> <a href="{{ route('databases') }}">Databases</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-computer"></i> <a href="{{ route('commands') }}">Commands</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-bank"></i> <a href="{{ route('logins') }}">User logins</a></li></div>
+                <div class="mt-2 mb-2"><li><button class="btn btn-success w-100"><i class="fas fa-tachometer-alt"></i> <a href="{{ url('logs') }}">System logs</a></li></div>
+                </div>  
+            </ul> </p>
+    </div>
+  </div>
 
-            <br/>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-database"></i> <a href="{{ route('databases') }}">Databases</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-computer"></i> <a href="{{ route('commands') }}">Commands</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-bank"></i> <a href="{{ route('logins') }}">User logins</a></li></div>
-            <div class="mt-2 mb-2"><li><button class="btn btn-success" style="width: 20rem; border: 1px solid"><i class="fas fa-tachometer-alt"></i> <a href="{{ url('logs') }}">System logs</a></li></div>
-            </div>  
-        </ul> 
+</div>
+</div>
+
     </div>
 </x-app-layout>
