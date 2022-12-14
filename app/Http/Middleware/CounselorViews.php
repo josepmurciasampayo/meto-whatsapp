c398c0ccb586d2a91d999fcfb4206fd4f42f76ab
josepmurciasampayo <josepmurciasampayo@gmail.com>
2022-12-14 11:01:24 -0500

    fix visibility for high school AP counselors

--- app/Http/Middleware/CounselorViews.php
+++ app/Http/Middleware/CounselorViews.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\HighSchool;
+use App\Models\Student;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -21,6 +22,7 @@ class CounselorViews
             return $next($request);
         }
         if (Auth()->user()->isCounselor()) {
+
             $params = $request->route()->parameters();
             if (isset($params['highschool_id'])) {
                 $this->checkSchool($params['highschool_id']);
@@ -31,6 +33,7 @@ class CounselorViews
             if (isset($params['student_id'])) {
                 $this->checkStudent($params['student_id']);
             }
+
             return $next($request);
         }
     }
@@ -45,15 +48,22 @@ class CounselorViews
 
     private function checkStudent(int $student_id) :void
     {
-        $studentSchool = HighSchool::getByStudentID($student_id);
-        $homeSchool = HighSchool::getByCounselorID(Auth()->user()->id);
+        if (Auth()->user()->isCounselorAtAP()) {
+            $ap_id = HighSchool::getByCounselorID(Auth()->user()->id)->id;
+            if (!HighSchool::isStudentEnrolled($student_id, $ap_id)) {
+                abort(403);
+            }
+        } else {
+            $studentSchool = HighSchool::getByStudentID($student_id);
+            $homeSchool = HighSchool::getByCounselorID(Auth()->user()->id);
 
-        if (is_null($studentSchool) || is_null($homeSchool)) {
-            abort(403);
-        }
+            if (is_null($studentSchool) || is_null($homeSchool)) {
+                abort(403);
+            }
 
-        if ($homeSchool->id != $studentSchool->id) {
-            abort(403);
+            if ($homeSchool->id != $studentSchool->id) {
+                abort(403);
+            }
         }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\HighSchool\Type;
 use App\Enums\User\Role;
 use App\Enums\HighSchool\Role as HSRole;
 use App\Helpers;
@@ -114,5 +115,17 @@ class User extends Authenticatable
             join meto_user_high_schools as h on h.user_id = u.id and h.role in (' . HSRole::ADMIN() . ', ' . HSRole::COUNSELOR() . ')
             where h.highschool_id = ' . $highschool_id .' ;
         ');
+    }
+
+    public function isCounselorAtAP() :bool
+    {
+        $row = Helpers::dbQueryArray('
+        select h.type
+        from meto_user_high_schools as uh
+        join meto_high_schools as h on uh.highschool_id = h.id
+        where user_id= ' . $this->id . ';
+        ');
+
+        return $row[0]['type'] == Type::ACCESS();
     }
 }
