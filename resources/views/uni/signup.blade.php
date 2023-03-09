c2a8715ef6c0163cee2d2c7f5c31e3618be2a748
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-03-09 13:46:26 +0000

    design uni sign-up view

--- resources/views/uni/signup.blade.php
+++ resources/views/uni/signup.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-    <h3>University and Scholarship Recruiters | Get Started</h3>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <x-image-with-text
+                           image-src="/img/Meto-background.webp"
+                           alt=""
+                           text="University and Scholarship Recruiters | Get Started"/>
+
+                           <div class="w-75 lg:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    
     <form method="POST" action="{{ route('signup.uni.store') }}">
         @csrf
         <x-input label="First Name" name="first"></x-input>
@@ -8,10 +16,23 @@
         <x-input label="Password" name="password"></x-input>
         <x-input label="Job Title" name="title"></x-input>
         <x-input label="Institution Name" name="institution"></x-input>
-        <x-button type="submit">Sign Up</x-button>
+        <div class="text-center my-4">
+            <x-button-icon type="submit" icon="" text="Sign Up" />
+          </div>
     </form>
-    <div class="text-center">
-    <p>Already have an account? <a href="">Log In</a></p>
-    <p>Please note that you will be able to add coworkers to your institution account once your request is approved.</p>
+
+    <div class="flex align-items-center justify-center mt-20 text-xm">
+        <p>Already have a Meto account?</p>
+        <span class="mx-2"></span>
+        <x-button-nav href="{{route('login') }}" class="btn btn-outline text-gray-600 hover:text-gray-900 text-xs text-center w-10"><i class="fa fa-sign-in"></i> Log In</x-button-nav>
+    </div>
+
+    <div class="flex align-items-center justify-center mt-5 text-xm">
+        <x-icon-link text="I am a Student" icon="fa fa-sync" href="#"/>
+    </div>
+
+   
+
+    <p class="text-xm text-center mt-5 font-semibold">Please note that you will be able to add coworkers to your institution account once your request is approved.</p>
     </div>
 </x-app-layout>
