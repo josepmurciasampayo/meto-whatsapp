9f986c7d547e78585bf4208c75f8e3e7e3ba6a70
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-03-09 14:56:01 +0000

    design for welcome uni view

--- resources/views/uni/thankyou.blade.php
+++ resources/views/uni/thankyou.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center">
-        <div class="text-green-600 mb-4">
+    <div class="min-h-screen items-center">
+        <div class="text-green-600 mb-4 text-center mt-6">
             <i class="fas fa-check-circle fa-10x"></i>
         </div>
-        <h2 class="display-4 font-bold mb-4">Success!</h2>
+        <h2 class="display-4 font-bold mb-4 mt-6 text-center">Success!</h2>
         <p class="text-center mb-4 display-7">Your request has been sent and a Meto representative will contact you soon.</p>
     </div>
 </x-app-layout>
+
 
 
--- resources/views/uni/welcome.blade.php
+++ resources/views/uni/welcome.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout>
-    <div class="text-center">
-        <h3>Hello!</h3>
-        <h4>Welcome to Meto, the online meeting place where you can efficiently connect with students.</h4>
-        <p>
-            We will now ask you a short series of questions. Your answers will help us ensure that your review
-        and connection process is as efficient as possible. It will take around 3 minutes.
-        </p>
-        <x-button onclick="window.location.assign('/uni-name')">Let's Go</x-button>
+    <div class="min-h-screen items-center">
+        <div class="text-center mt-6 text-xl">
+            <h3 class="mb-4 mt-6">Hello!</h3>
+            <h4 class="mb-4 mt-6 display-7">Welcome to Meto, the online meeting place where you can efficiently connect with students.</h4>
+            <p class="mb-4 mt-6 text-xl">We will now ask you a short series of questions. Your answers will help us ensure that your review
+            and connection process is as efficient as possible. It will take around 3 minutes.
+            </p>
+            <x-button onclick="window.location.assign('/uni-name')">Let's Go <i class="fas fa-arrow-right"></i></x-button>
+        </div>
     </div>
 </x-app-layout>
+
