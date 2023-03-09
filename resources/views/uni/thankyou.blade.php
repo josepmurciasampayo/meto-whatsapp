1f55dd2adefe857066d3000ba46ae66933493d9f
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-03-09 14:00:08 +0000

    design thankyou uni view

--- resources/views/uni/thankyou.blade.php
+++ resources/views/uni/thankyou.blade.php
@@ -1,3 +1,11 @@
 <x-app-layout>
-    <h2>Your request has been sent and a Meto representative will contact you soon.</h2>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center">
+        <div class="text-green-600 mb-4">
+            <i class="fas fa-check-circle fa-10x"></i>
+        </div>
+        <h2 class="display-4 font-bold mb-4">Success!</h2>
+        <p class="text-center mb-4 display-7">Your request has been sent and a Meto representative will contact you soon.</p>
+    </div>
 </x-app-layout>
+
+
