c3da61c1e04351f784c0c9cc6a00b5e2c0fd8d97
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-03-31 11:28:20 +0000

    add letter grade

---
+++ resources/views/components/letter-grades.blade.php
@@ -0,0 +1,29 @@
+@props(['name', 'label', 'saved' => '']) 
+
+
+<div>
+    <label class="text-lg font-medium text-gray-800 mb-2">{{ $label }}</label>
+    <div class="relative">
+        <select
+            id="{{ $name }}"
+            name="{{ $name }}"
+            class="block w-full pl-3 pr-10 py-2 rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-900 sm:text-sm"
+        >
+            <option value="">Select a Grade</option>
+            <option value="A" {{ $saved == 'A' ? 'selected' : '' }}>A</option>
+            <option value="A-" {{ $saved == 'A-' ? 'selected' : '' }}>A-</option>
+            <option value="B+" {{ $saved == 'B+' ? 'selected' : '' }}>B+</option>
+            <option value="B" {{ $saved == 'B' ? 'selected' : '' }}>B</option>
+            <option value="B-" {{ $saved == 'B-' ? 'selected' : '' }}>B-</option>
+            <option value="C+" {{ $saved == 'C+' ? 'selected' : '' }}>C+</option>
+            <option value="C" {{ $saved == 'C' ? 'selected' : '' }}>C</option>
+            <option value="C-" {{ $saved == 'C-' ? 'selected' :'' }}>C-</option>
+            <option value="D+" {{ $saved == 'D+' ? 'selected' : '' }}>D+</option>
+            <option value="D" {{ $saved == 'D' ? 'selected' : '' }}>D</option>
+            <option value="D-" {{ $saved == 'D-' ? 'selected' : '' }}>D-</option>
+            <option value="E" {{ $saved == 'E' ? 'selected' : '' }}>E</option>
+            <option value="F" {{ $saved == 'F' ? 'selected' : '' }}>F</option>
+            </select>
+            </div>
+            
+            </div>
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -76,5 +76,9 @@
             @if ($question->format === App\Enums\QuestionFormat::AP())
             <x-ap-subjects label="{!! $question->text !!}" name="{!! $question->id !!}" help="{!! $question->help !!}" saved="{!! $answer !!}"></x-ap-subjects>
             @endif
+
+            @if ($question->format === App\Enums\QuestionFormat::LETTERGRADE())
+            <x-letter-grades label="{!! $question->text !!}" name="{!! $question->id !!}" help="{!! $question->help !!}" saved="{!! $answer !!}"></x-letter-grades>
+            @endif
             
             </div>
