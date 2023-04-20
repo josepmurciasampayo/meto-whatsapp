b8d18c2310dc37c7140b56ef3c785796fbef90be
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-04-20 16:00:05 +0000

    fix number input for negative input

--- resources/views/components/inputs/number.blade.php
+++ resources/views/components/inputs/number.blade.php
@@ -8,6 +8,6 @@
     @endif
     <div class="flex flex-wrap items-center">
         @php $required = ($req == \App\Enums\General\YesNo::YES()) ? "required" : ""  @endphp
-        <input type="number" name="{{ $name }}" id="{{ $name }}" value="{{ $saved }}" class="mt-2 sm:mt-0 flex-1 ml-0 sm:ml-2 block w-full pr-10 pl-3 py-2 rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-900 sm:text-sm bg-white" pattern="\d{0,10}" {{ $required }}>
+        <input type="number" name="{{ $name }}" id="{{ $name }}" value="{{ $saved }}" class="mt-2 sm:mt-0 flex-1 ml-0 sm:ml-2 block w-full pr-10 pl-3 py-2 rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-900 sm:text-sm bg-white" pattern="\d{0,10}" {{ $required }} min="0">
     </div>
 </div>
