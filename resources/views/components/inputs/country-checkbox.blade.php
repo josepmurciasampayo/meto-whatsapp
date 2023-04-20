376bd033d70df50ad9c615906e2cc29908cc7361
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-04-20 15:45:58 +0000

    fix date format and limits for country checkbox

--- resources/views/components/inputs/country-checkbox.blade.php
+++ resources/views/components/inputs/country-checkbox.blade.php
@@ -4,6 +4,7 @@
 @php
     $countries = App\Models\EnumCountry::orderBy('name', 'asc')->get();
     $savedCountries = explode(',', $saved);
+    $checkedCount = 0;
 @endphp
 
 <div class="my-4 bg-gray-100 px-4 py-4 rounded-md">
@@ -13,10 +14,32 @@
     @endif
     <div class="bg-white px-2" style="max-height: 150px; overflow: auto">
         @foreach ($countries as $country)
-            <div>
-                <input type="checkbox" id="{{ $name }}[{{ $country->id }}]" name="{{ $name }}[{{ $country->id }}]" value="{{ $country->id }}" {{ in_array($country->id, $savedCountries) ? 'checked' : '' }} class="rounded text-green-600">
-                <label for="{{ $name }}[{{ $country->id }}]" class="ml-2">{{ $country->name }}</label>
-            </div>
+            @if ($checkedCount < 3)
+                <div>
+                    <input type="checkbox" id="{{ $name }}[{{ $country->id }}]" name="{{ $name }}[{{ $country->id }}]" value="{{ $country->id }}" {{ in_array($country->id, $savedCountries) ? 'checked' : '' }} class="rounded text-green-600" onclick="checkLimit(this)">
+                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2">{{ $country->name }}</label>
+                </div>
+            @else
+                <div>
+                    <input type="checkbox" id="{{ $name }}[{{ $country->id }}]" name="{{ $name }}[{{ $country->id }}]" value="{{ $country->id }}" {{ in_array($country->id, $savedCountries) ? 'checked' : '' }} class="rounded text-green-600" disabled>
+                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2">{{ $country->name }}</label>
+                </div>
+            @endif
         @endforeach
     </div>
 </div>
+
+<script>
+    function checkLimit(checkbox) {
+        if (checkbox.checked) {
+            if ($('input[type=checkbox]:checked').length > 3) {
+                checkbox.checked = false;
+                alert('You can only select up to 3 options.');
+            } else {
+                $checkedCount++;
+            }
+        } else {
+            $checkedCount--;
+        }
+    }
+</script>
