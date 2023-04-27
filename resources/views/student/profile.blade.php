7240150a9372724abe91f48023c30cb88cd09799
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-04-27 14:42:22 +0000

    fix profile label

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 mt-5">
         <x-image-with-text
                            image-src="/img/Meto-background.webp"
                            alt=""
-                           text="Profile"/>
+                           text="Basic Information"/>
         <div class="w-75 lg:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <form method="POST" action="{{ route('student.handle') }}">
                  <input-text type="hidden" name="page" value="{{ \App\Enums\Page::GETSTARTED() }}">
