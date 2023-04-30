85d3c5798a1d6f7669854cabd52463eed2c341a5
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-04-30 11:05:57 -0400

    required for checkboxes

--- resources/views/components/inputs/country-checkbox.blade.php
+++ resources/views/components/inputs/country-checkbox.blade.php
@@ -1,5 +1,5 @@
 <!-- resources/views/components/country-checkbox.blade.php -->
-@props(['label', 'name', 'help' => false, 'saved' => ''])
+@props(['label', 'name', 'help' => false, 'saved' => '', 'req' => false])
 
 @php
     $countries = App\Models\EnumCountry::orderBy('name', 'asc')->get();
@@ -8,21 +8,23 @@
 @endphp
 
 <div class="my-4 bg-gray-100 px-4 py-4 rounded-md">
-    <label class="text-lg font-medium text-gray-800 mb-2">{{ $label }}</label>
+    @php $required = ($req) ? '*' : '' @endphp
+    <label class="text-lg font-medium text-gray-800 mb-2">{{ $label }} {{ $required }}</label>
     @if ($help)
         <div class="text-sm text-gray-600 italic mb-4">{{ $help }}</div>
     @endif
     <div class="bg-white px-2" style="max-height: 150px; overflow: auto">
+        @php $required = ($req) ? 'required' : '' @endphp
         @foreach ($countries as $country)
             @if ($checkedCount < 5)
                 <div>
                     <input type="checkbox" id="{{ $name }}[{{ $country->id }}]" name="{{ $name }}[{{ $country->id }}]" value="{{ $country->id }}" {{ in_array($country->id, $savedCountries) ? 'checked' : '' }} class="rounded text-green-600" onclick="checkLimit(this)">
-                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2">{{ $country->name }}</label>
+                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2" {{ $required }}>{{ $country->name }}</label>
                 </div>
             @else
                 <div>
                     <input type="checkbox" id="{{ $name }}[{{ $country->id }}]" name="{{ $name }}[{{ $country->id }}]" value="{{ $country->id }}" {{ in_array($country->id, $savedCountries) ? 'checked' : '' }} class="rounded text-green-600" disabled>
-                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2">{{ $country->name }}</label>
+                    <label for="{{ $name }}[{{ $country->id }}]" class="ml-2" {{ $required }}>{{ $country->name }}</label>
                 </div>
             @endif
         @endforeach
--- resources/views/components/inputs/radio.blade.php
+++ resources/views/components/inputs/radio.blade.php
@@ -1,14 +1,16 @@
-@props(['options', 'name', 'help' => false, 'saved' => '', 'label' => '', 'required' => false])
+@props(['options', 'name', 'help' => false, 'saved' => '', 'label' => '', 'req' => false])
 
 <fieldset class="my-4 bg-gray-100 p-4 rounded-md">
-    <label class="text-lg font-medium text-gray-800 mb-2">{{ $label }}</label>
+    @php $required = ($req) ? '*' : '' @endphp
+    <label class="text-lg font-medium text-gray-800 mb-2">{{ $label }} {{ $required }}</label>
     @if ($help)
         <div class="text-sm text-gray-600 mb-4">{{ $help }}</div>
     @endif
     <div class="flex flex-col gap-2 bg-white p-2">
         @foreach ($options as $value => $option)
-            <?php $checked = ($option == $saved) ? 'checked' : '' ?>
-            <label class="inline-flex items-center">
+            @php $checked = ($option == $saved) ? 'checked' : '' @endphp
+            @php $required = ($req) ? 'required' : '' @endphp
+            <label class="inline-flex items-center" {{ $required }}>
                 <input type="radio" class="form-radio h-4 w-4 text-green-600" id="{{ $name . '[' . $value . ']' }}" name="{{ $name }}" value="{{ $value }}" {{ $checked }}>
                 <span class="ml-2 text-m">{{ $option }}</span>
             </label>
