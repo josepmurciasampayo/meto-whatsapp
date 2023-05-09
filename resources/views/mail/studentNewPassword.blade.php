943f89cee6393c1e6712bdb84bdedfabe4877519
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-05-09 16:47:08 +0000

    updated studentNewPassword email template

--- resources/views/mail/studentNewPassword.blade.php
+++ resources/views/mail/studentNewPassword.blade.php
@@ -1,22 +1,18 @@
 Dear {{ $user->first }},<br/>
-<br/>
-Greetings from Meto!<br/>
-<br/>
-In the past, you created a Meto profile. Since then, we upgraded our tech platform, and our hope (and expectation) is that it will make your experience far better.<br/>
-<br/>
-We kindly urge you to log in and verify the accuracy of your information. In light of updates to some of the questions, we also request that you complete your profile. To update basic details like your WhatsApp number and change your password, please visit your basic information page.<br/>
-<br/>
-To access your account, you will need to log in.<br/>
-<br/>
-Begin at <a href="https://app.meto-intl.org">https://app.meto-intl.org</a>.
-Your new account has been created using the email: {{ $user->email }}. To get started, use the temporary password below:<br/>
-<br/>
-Temporary Password: {{ $password }}<br/>
-<br/>
-For prompt assistance with any issues or questions, please submit a request for help here, or alternatively, email us at <a href="mailto:support@21913322.hubspot-inbox.com">support@21913322.hubspot-inbox.com</a>.<br/>
-<br/>
-Thank you for using Meto, and we look forward to providing an improved experience.<br/>
-<br/>
-Best regards,<br/>
-<br/>
-The Meto Team
+<br/> Greetings from Meto!<br/>
+<br/> We've upgraded our tech platform since you created your Meto profile. Your new profile looks fantastic, and we encourage you to check it out. Please log in to verify your info and complete your profile, considering updated questions. Rest assured, we retained most of your information.<br/>
+<br/> To keep your Meto profile up-to-date for universities, use the new portal instead of the old Google Form. Access the portal by following these steps:<br/>
+<ul>
+    <li>Visit <a href="https://app.meto-intl.org">https://app.meto-intl.org</a>
+
+        <li>Log in using the following:
+            <ul>
+                <li>Email: {{ $user->email }}</li>
+                <li>Temporary password: {{ $password }}</li>
+            </ul>
+        </li>
+    </ul>
+    <br/> For prompt assistance with any issues or questions, please submit a request for help <a href="https://app.meto-intl.org/contact">here</a> or email us at <a href="mailto:support@21913322.hubspot-inbox.com">support@21913322.hubspot-inbox.com</a>.<br/>
+    <br/> Thank you for using Meto, and we look forward to providing an improved experience.<br/>
+    <br/> Best regards,<br/>
+    <br/> The Meto Team
