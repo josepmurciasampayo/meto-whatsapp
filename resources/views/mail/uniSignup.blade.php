826ccfd19d4ff71ed737db1e08948a125e15c66c
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-05-13 14:56:54 -0400

    sign up email

--- resources/views/mail/uniSignup.blade.php
+++ resources/views/mail/uniSignup.blade.php
@@ -1,3 +1,7 @@
 New University Signup<br/>
 <br/>
-{{  }}
+{{ $name }} ({{ $title }}) requested more information for {{ $institution }}. You can contact them at <a href="mailto:{{ $email }}">{{ $email }}</a>.
+<br/><br/>
+They also wrote:<br/>
+{{ $how }}
+
