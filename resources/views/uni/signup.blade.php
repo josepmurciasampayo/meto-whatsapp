b289c7a0a34377d0f2365aba39372870aeb8ee70
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-05-17 18:37:47 +0000

    update uni sign-up language

--- resources/views/uni/signup.blade.php
+++ resources/views/uni/signup.blade.php
@@ -1,19 +1,22 @@
 <x-app-layout>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 mt-3">
         <x-image-with-text image-src="/img/Meto-background.webp" alt="" text="University and Scholarship Recruiters | Get Started"/>
         <div class="w-75 lg:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <p class="my-4">I think a couple sentences here about the sign-up process might make sense?</p>
+            <p class="text-xm text-center mt-5 font-semibold">Please complete the form below. Your response will be submitted to Meto for review, and then you will be granted access to Meto's database.</p>
+
             <form method="POST" action="{{ route('signup.uni.store') }}">
                 @csrf
-                <x-inputs.text label="Name" name="name"></x-inputs.text>
-                <x-inputs.text label="Institution Name" name="institution"></x-inputs.text>
-                <x-inputs.text label="Email Address" name="email"></x-inputs.text>
+                <x-inputs.text label="Name" name="name" help="Please enter your full name. This information will assist us in reviewing your request." req="true"></x-inputs.text>
+                <x-inputs.text label="Institution Name" name="institution" req="true"></x-inputs.text>
+                <x-inputs.text label="Email Address" name="email" req="true"></x-inputs.text>
                 <x-inputs.text label="Job Title" name="title"></x-inputs.text>
                 <x-inputs.textarea label="How did you hear about Meto and how are you looking to use Meto?" name="how"></x-inputs.textarea>
                 <div class="text-center my-4">
                     <x-button-icon type="submit" icon="" text="Sign Up" />
                 </div>
-            </form>
+            </form> 
+            
+            
 
             <div class="flex align-items-center justify-center mt-20 text-xm">
                 <p>Already have a Meto account?</p>
