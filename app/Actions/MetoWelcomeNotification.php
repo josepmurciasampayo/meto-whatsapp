6be90d240231b2428844d07f2021566a81432405
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-05-23 01:18:03 -0400

    updated language in uni welcome email

--- app/Actions/MetoWelcomeNotification.php
+++ app/Actions/MetoWelcomeNotification.php
@@ -25,6 +25,7 @@ class MetoWelcomeNotification extends WelcomeNotification
             ->line(Lang::get('Your Meto account has been created for :uni. Get started by logging in below to access the database and connect with students.', ['uni' => $this->uni->name]))
             ->action(Lang::get('Set initial password'), $this->showWelcomeFormUrl)
             ->line(Lang::get('Please note, your current account allows you to connect with :count students. If you would like to connect with more students, please initiate an account upgrade request by clicking "Increase Connections" on your database homepage', ['count' => $this->uni->connections]))
-            ->line(Lang::get('This welcome link will expire in :count hours.', ['count' => $this->validUntil->diffInRealHours()]));
+            ->line(Lang::get('This welcome link will expire in :count hours.', ['count' => $this->validUntil->diffInRealHours()]))
+            ->line(Lang::get('If this link expires, please contact us by reply email.'));
     }
 }
