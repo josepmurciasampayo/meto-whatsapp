ed0e69126efb79027d24689a1fb4b329fceecd88
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-06-08 15:14:50 +0000

    update student intro page

--- resources/views/student/intro.blade.php
+++ resources/views/student/intro.blade.php
@@ -5,7 +5,7 @@
           <h4 class="my-2 display-8">Welcome to Meto, the online ‘meeting place’ where you can connect efficiently with good-fit education opportunities.</h4>
           <div class="text-center">
             <p class="my-2 mx-auto">
-              You will now be required to answer a series of questions to create your Meto profile. Your answers will determine which universities or programs reach out to you, so please be as thorough and truthful as possible. It will take you about 30 minutes.
+              You will now be required to answer a series of questions to create your Meto profile. Your answers will determine which universities or programs reach out to you, so please be as thorough and truthful as possible. It will take you about 10 minutes.
             </p>
           </div>
           <a class="mt-6" href="{{ route('student.demographic') }}">
