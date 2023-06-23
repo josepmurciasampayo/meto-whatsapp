a255d2c937e3d0db953af45587de87a2012b0e77
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-06-24 00:32:37 +1000

    Added factory to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use App\Helpers;
 use App\Models\Chat\MessageState;
 use App\Models\Joins\UserHighSchool;
 use App\Models\Joins\UserInstitution;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +19,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, \Spatie\WelcomeNotification\ReceivesWelcomeNotification;
+    use HasApiTokens, HasFactory, Notifiable, \Spatie\WelcomeNotification\ReceivesWelcomeNotification;
 
     /**
      * The attributes that are mass assignable.
