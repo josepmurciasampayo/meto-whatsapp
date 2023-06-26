cdea74fcba9d1e2e985220d752e6d0e829ef3810
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-06-26 22:30:37 +1000

    Student register and uni user invite tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,13 +2,22 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Enums\General\YesNo;
+use App\Enums\User\Consent;
+use App\Enums\User\Role;
+use App\Enums\User\Status;
+use App\Mail\UniSignup;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     public function test_signup_screen_can_be_rendered()
     {
@@ -24,6 +33,33 @@ class RegistrationTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_new_student_users_can_register()
+    {
+        $email = $this->faker->email();
+        
+        $response = $this->post('/register', [
+            'first' => $this->faker->firstName(),
+            'middle' => $this->faker->lastName(),
+            'last' => $this->faker->lastName(),
+            'email' => $email,
+            'email_confirmation' => $email,
+            'phone' => [
+                'code' => '+61',
+                'number' => $this->faker->phoneNumber()
+            ],
+            'phone_owner' => 1,
+            'whatsapp_used' => ($this->faker->boolean()) ? YesNo::YES() : YesNo::NO(),
+            'whatsapp_consent' => ($this->faker->boolean()) ? Consent::CONSENT() : Consent::NOCONSENT(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'role' => Role::STUDENT(),
+            'status' => Status::ACTIVE(),
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
     public function test_signup_uni_screen_can_be_rendered()
     {
         $response = $this->get('/signup-uni');
@@ -31,21 +67,18 @@ class RegistrationTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_new_student_users_can_register()
-    {
-        /*$response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);*/
-    }
-
     public function test_new_uni_users_can_be_invited()
     {
-        //
+        Mail::fake();
+
+        $response = $this->post('/signup-uni', [
+            'name' => $this->faker->name(),
+            'institution' => $this->faker->company(),
+            'email' => $this->faker->email(),
+            'title' => $this->faker->jobTitle(),
+            'how' => $this->faker->text(200)
+        ]);
+
+        Mail::assertSent(UniSignup::class);
     }   
 }
