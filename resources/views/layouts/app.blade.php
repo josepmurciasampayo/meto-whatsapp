edc79a7a3cd88105a1ce05a0032b22473a71cd5d
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-06-27 19:32:28 +0100

    Try to fix getLIvewireId error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <script src="https://kit.fontawesome.com/c239959cd5.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script type="module">
+    <script type="module" defer>
         import Alpine from "https://cdn.jsdelivr.net/npm/alpinejs@3.12.2/+esm";
         // import TomSelect from "https://cdn.jsdelivr.net/npm/tom-select@2.2.2/+esm";
         import pgTomSelect from "{{ url('vendor/power-components/livewire-powergrid/resources/js/components/select/tomSelect.js') }}";
