56b7d1d2e3a34442fea86c4dd98647befe762aa9
josepmurciasampayo <josepmurciasampayo@gmail.com>
2023-07-04 20:34:37 +0100

    Centered uni questions

--- resources/views/uni/homepage.blade.php
+++ resources/views/uni/homepage.blade.php
@@ -3,13 +3,15 @@
     <div class="min-h-screen text-center">
         <x-image-with-text image-src="/img/Meto-background.webp" alt="" text=""/>
 
-        <form method="POST" action="{{ route('uni.homepage.store') }}" class="w-50">
-            @csrf
-            <h3 class="display-7 mb-1 mt-4">What is the website URL that will lead students to your institution's homepage?</h3>
-            <x-inputs.text saved="{{ $uni->url }}" name="url"></x-inputs.text>
-            <div class="mt-3">
-            <x-button-navigation/>
-            </div>
-        </form>
+        <div class="d-flex justify-content-center">
+            <form method="POST" action="{{ route('uni.homepage.store') }}" class="w-50">
+                @csrf
+                <h3 class="display-7 mb-1 mt-4">What is the website URL that will lead students to your institution's homepage?</h3>
+                <x-inputs.text saved="{{ $uni->url }}" name="url"></x-inputs.text>
+                <div class="mt-3">
+                    <x-button-navigation/>
+                </div>
+            </form>
+        </div>
     </div>
 </x-app-layout>
--- resources/views/uni/location.blade.php
+++ resources/views/uni/location.blade.php
@@ -1,31 +1,35 @@
 <x-app-layout>
     @section('title', config('app.name') . ' - University Location')
-    <div class="min-h-screen text-center">
-        <x-image-with-text image-src="/img/Meto-background.webp" alt="" text=""/>
 
-        <form method="POST" action="{{ route('uni.location.store') }}" class="w-50">
-        @csrf
-        <h3 class="display-7 mt-6">Country</h3>
-        @error('country')
-            <p class="text-danger my-0 py-0">{{ $message }}</p>
-        @enderror
-        <x-inputs.select saved="{{ $uni->country }}" name="country" :options="$countries" />
+    <div class="d-flex justify-content-center">
+        <div class="min-h-screen text-center w-50">
+            <x-image-with-text image-src="/img/Meto-background.webp" alt="" text=""/>
 
-        <h3 class="display-7 mt-6">State/Province</h3>
-        <p class="text-xm">leave blank if not applicable</p>
-        @error('state')
-            <p class="text-danger my-0 py-0">{{ $message }}</p>
-        @enderror
-        <x-inputs.text saved="{{ $uni->state }}" name="state"></x-inputs.text>
+            <form method="POST" action="{{ route('uni.location.store') }}" class="w-100">
+                @csrf
+                <h3 class="display-7 mt-6">Country</h3>
+                @error('country')
+                <p class="text-danger my-0 py-0">{{ $message }}</p>
+                @enderror
+                <x-inputs.select saved="{{ $uni->country }}" name="country" :options="$countries" />
 
-        <h3 class="display-7 mt-6">City</h3>
-        @error('city')
-            <p class="text-danger my-0 py-0">{{ $message }}</p>
-        @enderror
-        <x-inputs.text saved="{{ $uni->city }}" name="city"></x-inputs.text>
+                <h3 class="display-7 mt-6">State/Province</h3>
+                <p class="text-xm">leave blank if not applicable</p>
+                @error('state')
+                <p class="text-danger my-0 py-0">{{ $message }}</p>
+                @enderror
+                <x-inputs.text saved="{{ $uni->state }}" name="state"></x-inputs.text>
 
-            <div class="mt-3">
-        <x-button-navigation/>
-            </div>
-    </form>
+                <h3 class="display-7 mt-6">City</h3>
+                @error('city')
+                <p class="text-danger my-0 py-0">{{ $message }}</p>
+                @enderror
+                <x-inputs.text saved="{{ $uni->city }}" name="city"></x-inputs.text>
+
+                <div class="mt-3">
+                    <x-button-navigation/>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-app-layout>
